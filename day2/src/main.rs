c89f7822d2e39b4f096bd8c0eb62dca3f165b42e
Kuba Pawlaczyk <pawlaczyk.jakub@gmail.com>
2017-12-06 22:45:53 +0100

    Solution for day2

---
+++ day2/src/main.rs
@@ -0,0 +1,42 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn part1(input: &str) -> u32{
+    let mut sum = 0;
+
+    for line in input.lines() {
+        let line = line.split_whitespace();
+        let array: Vec<u32> = line.flat_map(|e| e.parse()).collect();
+        let max = array.iter().max().unwrap();
+        let min= array.iter().min().unwrap();
+
+        sum += max - min;
+    }
+    sum
+}
+
+fn part2(input: &str) -> u32 {
+    let mut sum = 0;
+
+    for line in input.lines() {
+        let line = line.split_whitespace();
+        let array: Vec<u32> = line.flat_map(|e| e.parse()).collect();
+
+        for i in 0..array.len() {
+            for j in 0..array.len() {
+                if array[i] % array[j] == 0 && i != j {
+                    sum += array[i] / array[j];
+                }
+            }
+        }
+    }
+
+    sum
+}
+
+fn main() {
+    let input = include_str!("../input");
+
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+}
